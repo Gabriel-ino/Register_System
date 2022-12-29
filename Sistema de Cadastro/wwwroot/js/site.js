7fd81f5ae82879d4476cc7d7cb02b0cc212fc770
Gabriel Chaves Martins <chavesmartinsg@gmail.com>
2022-12-29 19:14:49 -0300

    Paginação

--- Sistema de Cadastro/wwwroot/js/site.js
+++ Sistema de Cadastro/wwwroot/js/site.js
@@ -3,6 +3,11 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    $('#table-contacts').DataTable();
+});
+
+
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
