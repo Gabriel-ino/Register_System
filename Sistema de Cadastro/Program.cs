536b20fbb6065768bd0fc97f86cf13cd641f0b1e
Gabriel Chaves Martins <chavesmartinsg@gmail.com>
2023-01-01 13:07:05 -0300

    Style: Adicionando pagina de login

--- Sistema de Cadastro/Program.cs
+++ Sistema de Cadastro/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
